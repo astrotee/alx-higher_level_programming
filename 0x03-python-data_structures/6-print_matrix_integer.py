48279e091288374357da25d28cca10161fb5d4ee
amin ahmed <aminahmed24@gmail.com>
2024-01-06 19:59:54 +0200

    assert the matrix is a list of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not isinstance(matrix, list):
+        return
     for r in matrix:
-        print('{}'.format(' '.join(r)))
+        if not isinstance(r, list):
+            return
+        print('{:d}'.format(' '.join(r)))
