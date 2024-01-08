0d89f1130548d4b73b9926644bdcf98345fe961c
amin ahmed <aminahmed24@gmail.com>
2024-01-08 17:42:43 +0200

    use d format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         if not isinstance(r, list):
             return
         for c in r:
-            print('{}'.format(c), end="")
+            print('{:d}'.format(c), end="")
             if c != r[-1]:
                 print(" ", end="")
         print()
