7f7995bc489709dbf5d17c721feac5d137c7ef34
amin ahmed <aminahmed24@gmail.com>
2024-01-12 01:55:44 +0200

    return a new dict

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary: dict):
+    new_dict = dict()
     for k in a_dictionary:
-        a_dictionary[k] *= 2
-    return a_dictionary
+        new_dict[k] = a_dictionary[k] * 2
+    return new_dict
