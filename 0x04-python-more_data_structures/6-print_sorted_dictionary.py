b6b83b547f9c681f83f2cbef6e11966023d2f7ca
amin ahmed <aminahmed24@gmail.com>
2024-01-12 01:27:53 +0200

    FIX call keys and format methods

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary: dict):
     if a_dictionary is None:
         return
-    for k in sorted(a_dictionary.keys):
-        print("{}: {}", k, a_dictionary[k])
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[k]))
