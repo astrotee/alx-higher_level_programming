e994ec1742320d9f2819822cf467918a6c41c2c0
amin ahmed <aminahmed24@gmail.com>
2024-01-25 00:18:00 +0200

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            cnt += 1
+        except IndexError:
+            break
+    if cnt > 0:
+        print()
+    return cnt
