905660e8379a8e1b2b3e036c56aea208c1575cf7
amin ahmed <aminahmed24@gmail.com>
2024-01-24 21:31:55 +0200

    5. print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""define a Square class"""
+
+
+class Square:
+    """represents a square"""
+    def __init__(self, size=0):
+        """initialize the size"""
+        self.__size = size
+
+    def area(self):
+        """the area of the square"""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
