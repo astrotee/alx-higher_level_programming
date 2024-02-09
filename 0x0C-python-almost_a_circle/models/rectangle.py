ba6c287e30a59f0f898f1b3903c6a4216c246dd3
amin ahmed <aminahmed24@gmail.com>
2024-02-09 23:39:46 +0200

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,3 +65,14 @@ class Rectangle(Base):
     def area(self):
         """calculate the area"""
         return self.width * self.height
+
+    def display(self):
+        """display the rectangle"""
+        for _ in range(self.height):
+            print("#" * self.width)
+        if self.height == 0:
+            print()
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y}" \
+                f" - {self.width}/{self.height}"
