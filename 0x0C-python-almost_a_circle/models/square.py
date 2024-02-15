b78ee31bab75397023b0f69d6bb6ffa806281492
amin ahmed <aminahmed24@gmail.com>
2024-02-15 22:52:24 +0200

    use parent's update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,21 +21,17 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """update attributes"""
-        if len(args) == 0:
-            self.id = kwargs.get("id", self.id)
-            self.width = kwargs.get("size", self.width)
-            self.height = kwargs.get("size", self.height)
-            self.x = kwargs.get("x", self.x)
-            self.y = kwargs.get("y", self.y)
-        if len(args) == 1:
-            self.id = args[0]
-        if len(args) == 2:
-            self.width = args[1]
-            self.height = args[1]
-        if len(args) == 3:
-            self.x = args[2]
-        if len(args) == 4:
-            self.y = args[3]
+        if len(args) > 1:
+            nargs = args[:2] + args[1:]
+        else:
+            nargs = args
+        nkwargs = kwargs
+        if "size" in kwargs:
+            nkwargs["width"] = kwargs["size"]
+            nkwargs["height"] = kwargs["size"]
+            del nkwargs["size"]
+
+        super().update(*nargs, **nkwargs)
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
