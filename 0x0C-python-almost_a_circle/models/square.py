fc2752535468cfcec955b0ff4eacf251631de6fe
amin ahmed <aminahmed24@gmail.com>
2024-02-15 22:28:02 +0200

    12. square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,5 +19,23 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """update attributes"""
+        if len(args) == 0:
+            self.id = kwargs.get("id", self.id)
+            self.width = kwargs.get("size", self.width)
+            self.height = kwargs.get("size", self.height)
+            self.x = kwargs.get("x", self.x)
+            self.y = kwargs.get("y", self.y)
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.width = args[1]
+            self.height = args[1]
+        if len(args) == 3:
+            self.x = args[2]
+        if len(args) == 4:
+            self.y = args[3]
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
