90f9bff5d4b2990799d4ec7bc6e36f7182f75fad
amin ahmed <aminahmed24@gmail.com>
2024-02-15 23:15:05 +0200

    fix rectangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,15 +83,15 @@ class Rectangle(Base):
             self.height = kwargs.get("height", self.height)
             self.x = kwargs.get("x", self.x)
             self.y = kwargs.get("y", self.y)
-        if len(args) == 1:
+        if len(args) > 0:
             self.id = args[0]
-        if len(args) == 2:
+        if len(args) > 1:
             self.width = args[1]
-        if len(args) == 3:
+        if len(args) > 2:
             self.height = args[2]
-        if len(args) == 4:
+        if len(args) > 3:
             self.x = args[3]
-        if len(args) == 5:
+        if len(args) > 4:
             self.y = args[4]
 
     def to_dictionary(self):
