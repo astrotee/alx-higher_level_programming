cf40efe3050c77b9870dc170a6f0ebcd9ed1d69f
amin ahmed <aminahmed24@gmail.com>
2024-04-09 23:01:08 +0200

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const oldSquare = require('./5-square');
+
+class Square extends oldSquare {
+  charPrint (c) {
+    c ??= 'X';
+    for (let r = 0; r < this.height; r++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
