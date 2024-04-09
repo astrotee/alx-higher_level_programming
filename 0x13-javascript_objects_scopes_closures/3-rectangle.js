8512f8d991f3ff2f112bac9f47a4d366317dcfdf
amin ahmed <aminahmed24@gmail.com>
2024-04-09 22:23:35 +0200

    fix style

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        for(let r = 0; r < this.height; r++) {
-            console.log("X".repeat(this.width));
-        }
+  print () {
+    for (let r = 0; r < this.height; r++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 }
 
 module.exports = Rectangle;
