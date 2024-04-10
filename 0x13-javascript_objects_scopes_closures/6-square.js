f7855b9064cfee761a18e0daf71b6d4498d99b45
amin ahmed <aminahmed24@gmail.com>
2024-04-10 09:48:25 +0200

    fix 6-square.js for node 14

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const oldSquare = require('./5-square');
 
 class Square extends oldSquare {
   charPrint (c) {
-    c ??= 'X';
+    c ?? (c = 'X');
     for (let r = 0; r < this.height; r++) {
       console.log(c.repeat(this.width));
     }
