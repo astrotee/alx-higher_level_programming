28d512328a3b7d5c99561aa761c550a45040d101
amin ahmed <aminahmed24@gmail.com>
2024-04-21 17:51:03 +0200

    FIX 16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,9 +12,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    states = session.query(State).options(
-                selectinload(State.cities)
-                                          ).order_by(State.id)
+    states = session.query(State).order_by(State.id)
     for state in states:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
